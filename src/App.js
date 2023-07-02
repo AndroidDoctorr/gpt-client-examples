389ea073489af7292cdebfc76ed736a4889d9943
AndroidDoctorr <gbartthenerd@gmail.com>
2023-07-01 22:54:00 -0400

    created collapsible component to contain sections

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './styles/index.sass'
 import GPTClient from 'gpt-tools'
 import React, { useEffect } from 'react'
+import Collapsible from './components/Collapsible'
+import Installation from './components/Installation'
 
 const client = new GPTClient()
 const samplePrompt = 'Tell me a joke'
@@ -20,6 +22,12 @@ function App() {
         <h2>Examples</h2>
       </div>
 
+      <Collapsible
+        title={'Getting Started'}
+        content={<Installation />}
+        startCollapsed
+      />
+
       <div className='section'>
         <h3>Single Call</h3>
         <input id='singleCallInput' type='text' placeholder={samplePrompt} />
@@ -36,48 +44,6 @@ function App() {
         <hr></hr>
         <h4>Response:</h4>
         <p className='response' id='singleCallResponse'></p>
-      </div>
-
-      <div className='section'>
-        <h3>Getting Started</h3>
-        <h4>Account Setup</h4>
-        <p>
-          To obtain an <code>API KEY</code>, create an account at{' '}
-          <a href='https://platform.openai.com/signup'>OpenAI.com</a>
-        </p>
-        <p>
-          Create your key in the{' '}
-          <a href='https://platform.openai.com/account/api-keys'>API Keys</a>{' '}
-          section
-        </p>
-        <p>
-          <strong>
-            MAKE SURE TO COPY YOUR KEY as you won't get another chance to look
-            at it!
-          </strong>
-        </p>
-        <p>Put your key in the .env file(s) of your project, like so:</p>
-        <div className='codeSample'>
-          <code>{'REACT_APP_API_TOKEN=[API KEY]'}</code>
-        </div>
-        <h4>Installation</h4>
-        <p>Install this tool with npm or yarn</p>
-        <div className='codeSample'>
-          <code>{'npm install gpt-tools'}</code>
-        </div>
-        <br />
-        <div className='codeSample'>
-          <code>{'yarn add gpt-tools'}</code>
-        </div>
-        <h4>Usage</h4>
-        <p>Import the client class into your project and create an instance</p>
-        <div className='codeSample'>
-          <code>{"import GPTClient from 'gpt-tools'"}</code>
-          <br />
-          <code>{'...'}</code>
-          <br />
-          <code>{'const client = new GPTClient()'}</code>
-        </div>
       </div>
     </div>
   )
---
+++ src/components/Collapsible.js
@@ -0,0 +1,41 @@
+import React, { useState, useRef, useEffect } from 'react'
+import { FaAngleDown, FaAngleRight } from 'react-icons/fa'
+import './collapsible.sass'
+
+const Collapsible = ({ title, content, startCollapsed }) => {
+  const [isOpen, setIsOpen] = useState(!startCollapsed)
+  const contentRef = useRef(null)
+
+  useEffect(() => {
+    setIsOpen(!startCollapsed)
+  }, [startCollapsed])
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen)
+  }
+
+  const getContentHeight = () => {
+    if (contentRef.current) {
+      return isOpen ? `${contentRef.current.scrollHeight}px` : '0px'
+    }
+    return 'auto'
+  }
+
+  return (
+    <div className='collapsible'>
+      <button className='toggle-button' onClick={handleToggle}>
+        {isOpen ? <FaAngleDown /> : <FaAngleRight />}
+      </button>
+      <h3 className='title'>{title}</h3>
+      <div
+        className='content'
+        ref={contentRef}
+        style={{ height: !isOpen ? 0 : getContentHeight() }}
+      >
+        <div className='inner-content'>{content}</div>
+      </div>
+    </div>
+  )
+}
+
+export default Collapsible
