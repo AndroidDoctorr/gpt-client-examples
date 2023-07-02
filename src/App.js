45077d2091a41bed27742a2a4877891456216595
AndroidDoctorr <gbartthenerd@gmail.com>
2023-07-02 02:31:13 -0400

    created Agent component, copied from Conversation

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Collapsible from './components/Collapsible'
 import Installation from './components/Installation'
 import SingleChat from './components/SingleChat'
 import Conversation from './components/Conversation'
+import Agent from './components/Agent'
 
 const client = new GPTClient()
 
@@ -35,6 +36,24 @@ function App() {
         gptClient={client}
         conversation={[new ChatMessage('Knock knock!', Role.assistant)]}
         placeholder={"Who's there?"}
+      />
+      <Agent
+        title='Agent'
+        gptClient={client}
+        conversation={[
+          new ChatMessage(
+            'You are a Roman soldier in the late Republic era, serving in Gaul. ' +
+              'You are not far from your camp, fishing in a river. ' +
+              'You speak only Latin and some Greek.',
+            Role.system
+          ),
+          new ChatMessage('Salve! Quid agis?', Role.user),
+          new ChatMessage(
+            'Salve! Ego pescor. Tu quoque pescas?',
+            Role.assistant
+          ),
+        ]}
+        placeholder={"Non, lepores aucupor, vidistin'?"}
       />
     </div>
   )
---
+++ src/components/Agent.js
@@ -0,0 +1,54 @@
+import { React, useState } from 'react'
+import { ChatMessage, Role } from 'gpt-tools/dist/gpt-models'
+import RequestInput from './RequestInput'
+
+const Agent = ({
+  title,
+  gptClient,
+  placeholder,
+  messageLimit,
+  conversation,
+}) => {
+  const [prompt, setPrompt] = useState('')
+  const [messages, setMessages] = useState([...conversation])
+  const [isLoading, setIsLoading] = useState(false)
+
+  let limit = messageLimit || 5
+
+  const continueConversation = () => {
+    const requestMessage = new ChatMessage(prompt, Role.user)
+    messages.push(requestMessage)
+    setIsLoading(true)
+
+    gptClient.continueConversation(messages).then((response) => {
+      const responseMessage = new ChatMessage(response, Role.assistant)
+      messages.push(responseMessage)
+      setIsLoading(false)
+      setPrompt('')
+    })
+  }
+
+  return (
+    <div className='section'>
+      <h3>{title}</h3>
+      {messages.map((m, i) => (
+        <p
+          key={'message' + i}
+          className={m.role == Role.assistant ? 'response' : 'request'}
+        >
+          {m.content}
+        </p>
+      ))}
+      {messages.length <= limit && (
+        <RequestInput
+          placeholder={placeholder}
+          onSubmit={() => continueConversation()}
+          onChange={(p) => setPrompt(p)}
+          isLoading={isLoading}
+        />
+      )}
+    </div>
+  )
+}
+
+export default Agent
